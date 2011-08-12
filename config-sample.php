47c184fdb0237592382587acba74a6a8b690e7fc
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2011-08-12 04:06:23 -0400

    Change quoting style to single quotes

--- config-sample.php
+++ config-sample.php
@@ -29,50 +29,50 @@
 
 /* DATABASE INFORMATION */
 
-// database host (usually "localhost")
-define("CONFIG_DB_HOST", "localhost");
+// database host (usually 'localhost')
+define('CONFIG_DB_HOST', 'localhost');
 
-// database type (usually "mysql")
-define("CONFIG_DB_TYPE", "mysql");
+// database type (usually 'mysql')
+define('CONFIG_DB_TYPE', 'mysql');
 
 // database username
-define("CONFIG_DB_USER", "groundcontrol");
+define('CONFIG_DB_USER', 'groundcontrol');
 
 // database password
-define("CONFIG_DB_PASSWORD", "password");
+define('CONFIG_DB_PASSWORD', 'password');
 
 // database name
-define("CONFIG_DB_NAME", "groundcontrol");
+define('CONFIG_DB_NAME', 'groundcontrol');
 
-// database prefix (NOTE: If your server uses "database prefixes", in the format
-// of "username_databasename", put your prefix here, and we'll handle the rest.)
-define("CONFIG_DB_PREFIX", "ud");
+// database prefix (NOTE: If your server uses 'database prefixes', in the format
+// of 'username_databasename', put your prefix here, and we'll handle the rest.)
+define('CONFIG_DB_PREFIX', 'ud');
 
 // determine if a database prefix is in use; act accordingly
 if (strlen(CONFIG_DB_PREFIX) > 0)
 {
-    define("CONFIG_DB_FULLNAME", CONFIG_DB_PREFIX . "_" . CONFIG_DB_NAME);
+    define('CONFIG_DB_FULLNAME', CONFIG_DB_PREFIX . '_' . CONFIG_DB_NAME);
 }
 else
 {
-    define("CONFIG_DB_FULLNAME", CONFIG_DB_NAME);
+    define('CONFIG_DB_FULLNAME', CONFIG_DB_NAME);
 }
 
 /* DATE AND TIME CONFIGURATION */
 
 // default timezone (will be replaced by user's settings, when logged in)
-define("CONFIG_TIMEZONE", -5);
+define('CONFIG_TIMEZONE', -5);
 
 // default DST setting (will be replaced by user's settings, when logged in)
-define("CONFIG_DST", true);
+define('CONFIG_DST', true);
 
 /* PATHS */
-//define("PATH_ROOT", getcwd() . "/");
-define("PATH_API", PATH_ROOT . "api/");
-define("PATH_INC", PATH_ROOT . "inc/");
+//define('PATH_ROOT', getcwd() . '/');
+define('PATH_API', PATH_ROOT . 'api/');
+define('PATH_INC', PATH_ROOT . 'inc/');
 
 /* INCLUDES */
-include(PATH_INC . "DatabaseManager.inc.php");
-include(PATH_INC . "SensorManager.inc.php");
+include(PATH_INC . 'DatabaseManager.inc.php');
+include(PATH_INC . 'SensorManager.inc.php');
 
 ?>
